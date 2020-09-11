4dfec2b2f0e039d6c22d37dae25dfad488315292
Devan <devankav7@gmail.com>
2020-09-11 15:13:39 -0400

    stubbed out things

---
+++ dna.py
@@ -0,0 +1,10 @@
+
+class dna():
+    def __init__(self, genome):
+        self.genome = genome
+
+    def crossover(self):
+        pass
+
+    def fitness(self):
+        pass
---
+++ dna_test.py
@@ -0,0 +1,6 @@
+import dna
+
+def run_all_tests():
+    pass
+
+run_all_tests()
