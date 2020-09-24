01d4ae21f407b59856c99e06fade710186b47e06
Devan <devankav7@gmail.com>
2020-09-24 00:07:02 -0400

    added comments to tests

--- globals.py
+++ globals.py
@@ -1,7 +1,22 @@
+"""
+Contains variables and functions that are referenced and used acrossed multiple files
+in this project.
+
+author: Devan Kavalchek
+"""
+
 MUTATION_RATE = 0.01
 charset = 'abcdefghijklmnopwrstuvwxyz '
 
 def change_string_index(string, index, new_char):
+    """
+    Makes strings mutable. Replaces a string's character at a given index.
+
+    Parameters:
+        string: The string to be changed
+        index: The index that is being changed
+        new_char: The character that index is being changed to
+    """
     # Setup
     length = len(string)
     new_string = ""
@@ -9,12 +24,12 @@ def change_string_index(string, index, new_char):
     second_half = ""
 
     if index == 0: # If the index is the first character there won't be a first half
-        second_half = string[1:length]
+        second_half = string[1:length] # Set the second half to every letter except the first letter
     elif index == length - 1: # If the index is the last character there won't be a second half
-        first_half = string[0:length-1]
+        first_half = string[0:length-1] # Set the first half to every letter except the last letter
     else:
-        first_half = string[0:index]
-        second_half = string[index+1:length]
+        first_half = string[0:index] # Set the first half to all the characters before index
+        second_half = string[index+1:length] # Set the second half to all the characters after index
 
-    new_string = first_half + new_char + second_half
+    new_string = first_half + new_char + second_half # Create the new string by sandwiching the new character between the first and second halves
     return new_string
--- tests/dna_tests.py
+++ tests/dna_tests.py
@@ -7,6 +7,9 @@ from dna import *
 import globals
 
 def test_dna_init():
+    """
+    Tests that the dna class properly creates an offspring
+    """
     # Setup
     genome = "This is my genome"
 
@@ -19,6 +22,9 @@ def test_dna_init():
     print("test_dna_init passed!") # Prints if the test passess
 
 def test_dna_fitness():
+    """
+    Tests that the dna is accurately analyzing the function of each genome
+    """
     # Setup
     genome = "00c0e000i"
     target = "abcdefghi"
@@ -32,6 +38,9 @@ def test_dna_fitness():
     print("test_dna_fitness passed!") # Prints if the test passes
 
 def test_dna_crossover():
+    """
+    A test that ensures the DNA's crossover function is working properly
+    """
     # Setup
     # Create 2 offsprings with different genomes
     parent_1 = dna("00000000", "taargeet")
@@ -47,13 +56,16 @@ def test_dna_crossover():
 
     assert(offspring_genome == expected_genome)
 
-    print("test_dna_crossover passed!")
+    print("test_dna_crossover passed!") # Prints if the test passes
 
 def test_mutation():
+    """
+    A characterization test that tests that the DNA mutates properly
+    """
     # Setup
-    old_mutation_rate = globals.MUTATION_RATE
-    globals.MUTATION_RATE = 1
-    random.seed(1290809)
+    old_mutation_rate = globals.MUTATION_RATE # Remember the current mutation rate
+    globals.MUTATION_RATE = 1 # Set the mutation rate to 100% so that the function can be predictably tested
+    random.seed(1290809) # Set the seed so that the function can be predictably tested
     parent_1 = dna("00000000", "taargeet")
     parent_2 = dna("11111111", "taargeet")
     expected_genome = "eyorkowx"
@@ -64,9 +76,9 @@ def test_mutation():
     # Analyze
     assert(offspring.genome == expected_genome)
 
-    globals.MUTATION_RATE = old_mutation_rate
+    globals.MUTATION_RATE = old_mutation_rate # Set the mutation rate back to normal
 
-    print("test_mutation passed!")
+    print("test_mutation passed!") # Prints if the test passes
 
 def run_all_tests():
     """
