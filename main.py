9be04e4f2c8c62734f7c909f249b09b72c7c006d
Devan <devankav7@gmail.com>
2020-09-24 13:34:03 -0400

    working on a sorted population list by fitness. just need to workout a few bugs

--- main.py
+++ main.py
@@ -25,7 +25,69 @@ def main():
         offspring = dna(random_genome, target) # Create the dna object
         population.append(offspring) # Add the offspring to the population
 
+    
+    # Continue to breed new generations until the optimal genome is found
 
+    optimal_genome_found = False
+
+    while not optimal_genome_found:
+        mating_pool = []
+        ordered_population = []
+        optimal_genome_found = True
+
+        # Create a list of all offspring ordered by their fitness
+        for i in range(0, POPULATION_SIZE):
+            print(i)
+            offspring = population[i]
+            offspring_fitness = offspring.fitness()
+
+            if i == 0:
+                ordered_population.append(population[i])
+            elif i == 1:
+                if ordered_population[0].fitness() >= offspring_fitness:
+                    ordered_population.insert(0, offspring)
+                else:
+                    ordered_population.append(offspring)
+            else:
+                found_index = False
+                index = int(len(ordered_population)/2)
+
+                while not found_index:
+                    if index < 0 or index >= len(ordered_population):
+                        print("error")
+                        break
+                    elif index == 0:
+                        if ordered_population[index + 1].fitness() >= offspring_fitness:
+                            found_index = True
+
+                            ordered_population.insert(index, offspring)
+                        else:
+                            index += 1
+                    elif index == len(ordered_population) - 1:
+                        if ordered_population[index - 1].fitness() <= offspring_fitness:
+                            found_index = True
+
+                            ordered_population.insert(index, offspring)
+                        else:
+                            index -= 1
+                    else:
+                        if ordered_population[index].fitness() <= offspring_fitness:
+                            if ordered_population[index + 1].fitness() >= offspring_fitness:
+                                found_index = True
+
+                                ordered_population.insert(index, offspring)
+                            else:
+                                index += 1
+                        else:
+                            if ordered_population[index - 1].fitness() <= offspring_fitness:
+                                found_index = True
+                                
+                                ordered_population.insert(index - 1, offspring)
+                            else:
+                                index -= 1
+
+    for offspring in ordered_population:
+        print(offspring.fitness())
 
 if __name__ == "__main__":
     main()
