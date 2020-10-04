56c26f4cf16f84fc49dcc93aa45384e0d03382c3
Devan <devankav7@gmail.com>
2020-10-04 17:17:11 -0400

    finished salesman

--- main.py
+++ main.py
@@ -89,6 +89,8 @@ def salesman_algorithm():
     # Setup
     POPULATION_SIZE = salesman_settings.POPULATION_SIZE
     population = []
+    next_generation = []
+    first_generation = True
 
     # Initialize the points. In this case, the map is a circle
     radius = 20 # The radius of the circle
@@ -100,7 +102,7 @@ def salesman_algorithm():
     for x in range(0, num_points):
         percent = x/num_points
         pos = radians * percent
-        points.append([math.sin(pos), math.cos(pos)])
+        points.append([math.sin(pos) * radius, math.cos(pos) * radius])
 
     # Create inital population: N offspring with random genomes
     for _ in range(0, POPULATION_SIZE): # Create N offspring
@@ -116,11 +118,57 @@ def salesman_algorithm():
         population.append(offspring) # Add the offspring to the population
 
     # Continue to breed new generations until the optimal genome is found
+    optimal_genome_found = False
+    count = 0
+    generation_cap = salesman_settings.generation_cap
+
+    while not optimal_genome_found:
+        count += 1
+
+        if generation_cap is not False:
+            if count > generation_cap:
+                print("Generation cap was reached")
+                break
+        
+        if not first_generation:
+            population = next_generation # Set the current population to the new population
+            next_generation = [] # Reset next_generation
+        else:
+            first_generation = False
+
+        ordered_population = merge_sort(population)
+        most_fit = ordered_population[0] # Lower is better in the salesman problem
+
+        if not generation_cap:
+            if salesman_settings.target >= most_fit.fitness():
+                optimal_genome_found = True
+
+        print("Most fit:",most_fit.fitness())
+        print("Least fit:", ordered_population[len(ordered_population) - 1].fitness())
+
         # Choose N mates
-            # When choosing mates with weighted percent, DONT FORGET TO USE DECREASING_FUNCTION!!!!
+        MATING_POOL_SIZE = salesman_settings.MATING_POOL_SIZE
+        mating_pool = []
+
+        for _ in range(0, MATING_POOL_SIZE):
+            random_mate = weighted_element(ordered_population, globals.decreasing_function) # Use the decreasing function since the most fit offspring will be at the beginning of the array
+            mating_pool.append(random_mate)
+            ordered_population.remove(random_mate) # Remove so they are not chosen twice in a row
         # Create a new generation
-    
-    pass
+        children = POPULATION_SIZE // MATING_POOL_SIZE # How many children each offspring needs to have to repopulate the population
+        
+        for mate in mating_pool:
+            for _ in range(0, children): # Create the correct number of offspring to repopulate the population
+                # Pick a random mate
+                random_index = random.randrange(0, len(mating_pool))
+                random_mate = mating_pool[random_index]
+                
+                # Create a new offspring
+                offspring = mate.crossover(random_mate)
+                next_generation.append(offspring)
+
+    if optimal_genome_found:
+        print("Success! It took", count, "generations!")
 
 def main():
    #shakespearean_algorithm()
